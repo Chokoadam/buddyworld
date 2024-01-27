9a1f0b04d9a447f245a5d4fb17332cca9f3d49b0
Adam Vasquez <vasquez.bait@gmail.com>
2024-01-27 17:29:17 -0600

    Update buddyworld.cpp

--- buddyworld.cpp
+++ buddyworld.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 
 string palName;
+int inputColor;
 string color;
 bool lucky;
 string nature;
 
 void main(){
-    cout << "This is buddy world lol" << endl;
+    cout << "What do you want to name your buddy? :)" << endl;
+    cin >> palName;
+
+    cout << "\nWhat color do you want your pal to be? Please choose from the selection below:" << endl;
+    cout << "[1]: Red" << endl;
+    cout << "[2]: Blue" << endl;
+    cout << "[3]: Greem" << endl;
+    cout << "[4]: Yellow" << endl;
+    cout << "[5]: White" << endl;
+
 }
